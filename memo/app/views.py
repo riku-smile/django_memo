0a94e4541704da030b1dd70fc7acc4d2350efc5e
Riku <smile6280@icloud.com>
2021-08-10 23:12:57 +0900

    Add detail and new_memo

--- memo/app/views.py
+++ memo/app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Memo
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'app/index.html')
+
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    return render(request, 'app/detail.html', {'memo': memo})
+
+
+def new_memo(request):
+    return render(request, 'app/new_memo.html')
