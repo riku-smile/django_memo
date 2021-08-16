9dc847b73ed9869d59f8aa0af33bf84a4da8712a
Riku <smile6280@icloud.com>
2021-08-16 17:59:29 +0900

    create_form

--- memo/app/views.py
+++ memo/app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from memo.app.forms import MemoForm
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.http import require_POST
+from .forms import MemoForm
 from .models import Memo
-from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -15,4 +17,30 @@ def detail(request, memo_id):
 
 
 def new_memo(request):
-    return render(request, 'app/new_memo.html')
+    if request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:
+        form = MemoForm
+    return render(request, 'app/new_memo.html', {'form': form})
+
+
+@require_POST
+def delete_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    memo.delete()
+    return redirect('app:index')
+
+
+def edit_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    if request.method == 'POST':
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'app/edit_memo.html', {'form': form, 'memo': memo})
